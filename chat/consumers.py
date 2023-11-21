4a7a78332630187e7b3055247f9e5ed2eac88f65
UkiDelly <daehyeon1999@naver.com>
2023-11-21 22:26:48 +0900

    🎨 웹소켓 연결시 발생하는 로직 수정

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,19 +1,24 @@
 import json
 
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
-from django.contrib.auth.models import User
+
+from accounts.models import MyUser
 
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
+
     async def connect(self):
         room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        user: User = self.scope["user"]
-        # if self.scope["user"].is_authenticated:  # self.scope["user"].is_anonymous:
-        #     await self.close()
-        # else:
-        print(user.is_authenticated)
-        await self.accept()
-        await self.send("소켓 연결이 되었습니다.")
+        user: MyUser = self.scope["user"]
+        print(user)
+        if user.is_authenticated:
+            await self.accept()
+            await self.send("소켓 연결이 되었습니다.")
+        else:
+            await self.close()
+
+    async def disconnect(self, close_code):
+        await self.send("소켓 연결이 종료되었습니다.")
 
     async def receive_json(self, content, **kwargs):
         print(content)
