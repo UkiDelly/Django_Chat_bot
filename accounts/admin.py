cea7951b16e682761e348ca696b37543b52cff8f
UkiDelly <daehyeon1999@naver.com>
2023-11-21 11:19:46 +0900

    ✨ 어드민에 MyUser 등록

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from accounts.models import MyUser
+
+
 # Register your models here.
+@admin.register(MyUser)
+class MyUserAdmin(admin.ModelAdmin):
+    list_display = ["nickname", "email", "social_type", "created_at"]
+    list_filter = ['social_type']
+    readonly_fields = ['password']
+    search_fields = ['nickname', 'email', 'social_type']
+    ordering = ['created_at', 'social_type']
