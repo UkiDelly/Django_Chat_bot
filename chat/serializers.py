d3dff314ca0f2b5afaf69472087c8180975da9f2
UkiDelly <daehyeon1999@naver.com>
2023-11-22 12:05:54 +0900

    👽 chat_room 디테일 API 개발

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
+
 from rest_framework import serializers
 
-from chat.models import ChatRoom, ChatHistory
+from chat.models import ChatRoom, ChatHistory, SystemPromp
 
 
 class ChatRoomDto(serializers.ModelSerializer):
@@ -13,9 +15,24 @@ class ChatRoomDto(serializers.ModelSerializer):
 
 class CreateChatRoomDto(serializers.Serializer):
     name = serializers.CharField(max_length=100)
+    system_prompt = serializers.CharField(max_length=None)
+
+
+class SystemPrompDto(serializers.ModelSerializer):
+    class Meta:
+        model = SystemPromp
+        fields = ["id", "content", "created_at"]
 
 
 class ChatHistoryDto(serializers.ModelSerializer):
     class Meta:
         model = ChatHistory
-        fields = ["id", "name", "created_at"]
+
+        fields = ["id", "message", "role", "created_at"]
+
+
+@dataclass
+class ChatRoomInfoDto:
+    chat_room: dict
+    system_prompt: list[dict]
+    chat_history: list[dict]
--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,13 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, Http404
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
-from chat.models import ChatRoom
-from chat.serializers import ChatRoomDto, CreateChatRoomDto
+from chat.models import ChatRoom, SystemPromp, ChatHistory
+from chat.serializers import ChatRoomDto, CreateChatRoomDto, SystemPrompDto, ChatRoomInfoDto, ChatHistoryDto
 
 
 # Create your views here.
@@ -26,9 +29,41 @@ class ChatRoomViewSet(ModelViewSet):
 
         if serializer.is_valid():
             chat_room = ChatRoom.objects.create(user=request.user, name=serializer.data["name"])
+            chat_room.save()
+            SystemPromp.objects.create(content=serializer.data["system_prompt"], chat_room=chat_room).save()
             return Response(serializer.data, status=201)
         else:
             return Response(serializer.errors, status=400)
+
+    def retrieve(self, request: HttpRequest, *args, **kwargs):
+        instance = self.get_object()
+        instance_serializer = self.get_serializer(instance)
+
+        promps = SystemPromp.objects.filter(chat_room=instance)
+        promps_serializer = SystemPrompDto(promps, many=True)
+
+        chat_history = ChatHistory.objects.filter(chat_room=instance)
+        chat_history_serializer = ChatHistoryDto(chat_history, many=True)
+
+        dto = ChatRoomInfoDto(
+            instance_serializer.data,
+            promps_serializer.data,
+            chat_history_serializer.data
+        )
+
+        data = {"chat_room": dto.chat_room, "system_prompt": dto.system_prompt, "chat_history": dto.chat_history}
+        return Response(data, status=200)
+
+
+class ChatHistoryApiView(APIView):
+    http_method_names = ["get", 'post']
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        try:
+            chat_room = get_object_or_404(ChatRoom.objects.all(), pk=kwargs.get("room_id"))
+        except Http404:
+            return Response({"message": "존재하지 않는 채팅방입니다."}, status=HTTP_404_NOT_FOUND)
 
 
 class ChatHistoryViewSet(ModelViewSet):
